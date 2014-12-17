4c13eff5eaa22248b385aeab85221bb0e9d5f51f
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-12-17 15:17:59 +0100

    prepare for next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.spootnik/constance "0.5.0"
+(defproject org.spootnik/constance "0.5.1-SNAPSHOT"
   :description "constant time comparison"
   :url "https://github.com/pyr/constance"
   :license {:name "Eclipse Public License"
