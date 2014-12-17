f610d32810cb3f1fe530fe2eada5cce3d8e23066
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-12-17 15:29:21 +0100

    simplify

--- src/org/spootnik/constance.clj
+++ src/org/spootnik/constance.clj
@@ -1,19 +1,14 @@
 (ns org.spootnik.constance
   "Constant time comparison functions. Use when needing to avoid
-   timing attacks.")
+   timing attacks. See: http://codahale.com/a-lesson-in-timing-attacks/")
 
 (defn ^Boolean constant=
   "An iterative, constant-time comparison function for byte-arrays"
   [^"[B" e1 ^"[B" e2]
-  (boolean
-   (when (= (count e1) (count e2))
-     (loop [res        0
-            elems     (count e1)
-            [b1 & r1] (seq e1)
-            [b2 & r2] (seq e2)]
-       (if (pos? elems)
-         (recur (bit-or res (bit-xor b1 b2)) (dec elems) r1 r2)
-         (= res 0))))))
+  (and (boolean e1)
+       (boolean e2)
+       (= (count e1) (count e2))
+       (zero? (reduce bit-or 0 (map bit-xor e1 e2)))))
 
 (defn ^Boolean constant-string=
   "An iterative, constant-time comparison function for strings"
