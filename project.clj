da6626e7978f2d46113e80c628aa8cc828ae61c1
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-02-13 11:48:13 +0100

    project: warn on reflection

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "constant time comparison"
   :url "https://github.com/pyr/constance"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.2"]]}})
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.2"]]
+                   :global-vars {*warn-on-reflection* true}}})
