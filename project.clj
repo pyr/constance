cfa15856fd33e9f72266153e33991fb555b5b970
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-01-05 22:45:53 +0100

    project: 0.5.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/constance "0.5.4-SNAPSHOT"
+(defproject spootnik/constance "0.5.4"
   :description "constant time comparison"
   :url "https://github.com/pyr/constance"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.2"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]
                    :global-vars {*warn-on-reflection* true}}})
